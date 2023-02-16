774a7f7d536b80d405e1d4aa82785a1134d55512
Finn Kuusisto <finn.kuusisto@gmail.com>
2023-02-16 01:09:36 -0600

    Images in LOVE.

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 local listOfShapes = {}
 
 function love.load()
+  love.graphics.setBackgroundColor(1, 0, 1)
   tick = require "lib/tick/tick"
   Object = require("lib/classic/classic")
   require("shape")
   require("rectangle")
   require("circle")
+  
+  sheep = love.graphics.newImage("assets/img/sheep.png")
+  sheep_w = sheep:getWidth()
+  sheep_h = sheep:getHeight()
   
   tick.recur(createShape, 1)
 end
@@ -19,6 +24,11 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.draw(sheep, 100, 100, 0, 1, 1, sheep_w/2, sheep_h/2)
+  love.graphics.setColor(1, 0.78, 0.15, 0.5)
+  love.graphics.draw(sheep, 100, 100, math.pi/2, 1, 1, sheep_w/2, sheep_h/2)
+  love.graphics.setColor(1, 1, 1, 1)
+  
   for i = 1,#listOfShapes do
     local shp = listOfShapes[i]
     shp:draw()
