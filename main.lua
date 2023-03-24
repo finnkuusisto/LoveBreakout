1306929dbb80eee635d0fb8a7e9dff6d8d27c8b9
Finn Kuusisto <finn.kuusisto@gmail.com>
2023-03-23 21:59:46 -0500

    Started Sheepolution tutorial chap 14.

--- main.lua
+++ main.lua
@@ -1,66 +1,18 @@
-local listOfShapes = {}
-
 function love.load()
-  love.graphics.setBackgroundColor(1, 0, 1)
-  tick = require "lib/tick/tick"
   Object = require("lib/classic/classic")
-  require("shape")
-  require("rectangle")
-  require("circle")
+  require("player")
   
-  sheep = love.graphics.newImage("assets/img/sheep.png")
-  sheep_w = sheep:getWidth()
-  sheep_h = sheep:getHeight()
-  
-  main_rect = Rectangle(100, 100)
-  
-  --tick.recur(createShape, 1)
+  player = Player()
 end
 
 function love.update(dt)
-  tick.update(dt)
---  for i = 1,#listOfShapes do
---    local shp = listOfShapes[i]
---    shp:update(dt)
---  end
-  main_rect:update(dt)
+  player:update(dt)
 end
 
 function love.draw()
-  love.graphics.draw(sheep, 100, 100, 0, 1, 1, sheep_w/2, sheep_h/2)
-  love.graphics.setColor(1, 0.78, 0.15, 0.5)
-  love.graphics.draw(sheep, 100, 100, math.pi/2, 1, 1, sheep_w/2, sheep_h/2)
-  love.graphics.setColor(1, 1, 1, 1)
-  
-  for i = 1,#listOfShapes do
-    local shp = listOfShapes[i]
-    -- draw it different color if rectangle and intersecting
-    if shp:shape() == "rectangle" and shp:collidesWith(main_rect) then
-        love.graphics.setColor(1, 0, 0, 1)
-    end
-    shp:draw()
-    love.graphics.setColor(1, 1, 1, 1)
-  end
-  main_rect:draw()
+  player:draw()
 end
 
 function love.keypressed(key)
-  if key == "space" then
-    createShape()
-  end
-  if key == "escape" then
-    love.event.quit()
-  end
-end
-
-function createShape()
-  local x = math.random(250, 350)
-  local y = math.random(200, 300)
-  local shp = nil
-  if math.random() < 0.5 then
-    shp = Rectangle(x, y)
-  else
-    shp = Circle(x, y)
-  end
-  table.insert(listOfShapes, shp)
+  
 end
---
+++ player.lua
@@ -0,0 +1,33 @@
+Player = Object:extend()
+
+function Player:new()
+  self.image = love.graphics.newImage("assets/img/panda.png")
+  self.x = 300
+  self.y = 20
+  self.speed = 500
+  self.width = self.image:getWidth()
+end
+
+function Player:update(dt)
+  -- move left and right with keys
+  local moveDelta = dt * self.speed
+  if love.keyboard.isDown("right") then
+    self.x = self.x + moveDelta
+  end
+  if love.keyboard.isDown("left") then
+    self.x = self.x - moveDelta
+  end
+  -- but don't go out of frame
+  if self.x < 0 then
+    self.x = 0
+  end
+  local wind_width = love.graphics.getWidth()
+  if self.x > (wind_width - self.width) then
+    self.x = wind_width - self.width
+  end
+  
+end
+
+function Player:draw()
+  love.graphics.draw(self.image, self.x, self.y)
+end
